56917a69abb6e73b7c42d95dfad30fe26a3c4afe
BrandonF <37319101+zed71@users.noreply.github.com>
2020-04-20 21:51:07 -0700

    Add JS Folder

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_1.js
@@ -0,0 +1,10 @@
+window.alert("Brandons Page")
+
+var var1 = ("The story of a man who fell in the sea <br>");
+var var2 = ("He got eaten by the mighty Goldfish Tealeaf and lived for 20 "
++ "years in his stomach.");
+var var1 = var1.fontcolor("red")
+var fulltext = var1 + '' + var2;
+document.write(fulltext);
+
+document.write (100*125)
