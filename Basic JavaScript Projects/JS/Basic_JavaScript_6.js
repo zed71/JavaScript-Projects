ab918f036b5ed5db1814589873fa81206eab9574
BrandonF <37319101+zed71@users.noreply.github.com>
2020-05-03 18:44:08 -0700

    Add 128 assignment

--- Basic JavaScript Projects/JS/Basic_JavaScript_6.js
+++ Basic JavaScript Projects/JS/Basic_JavaScript_6.js
@@ -11,14 +11,6 @@ function Vehicle (Make, Model, Year, Color) {
     this.Vehicle_Year = Year;
     this.Vehicle_Color = Color;
 }
-var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
-var Emily = new Vehicle("Jeep", "Trail Hawk",  2019,  "White and Black");
-var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
-function myFunction() {
-    document.getElementById("Keywords_and_Constructors").innerHTML = 
-    "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model +
-    " manufactured in " + Erik.Vehicle_Color;
-}
 
 function Game (Platform, Genre, Year, Rated) {
     this.Game_Platform = Platform;
@@ -42,5 +34,15 @@ function count_Function() {
         function Plus_one() {Starting_point += 1;}
         Plus_one();
         return Starting_point;
+    }
+}
+
+function rocker_Function() {
+    document.getElementById("Nested_Function").innerHTML = Count();
+    function Count() {
+        var Start_here = 5;
+        function Minus_two() {Start_here -= 2;}
+        Minus_two();
+        return Start_here;
     }
 }
