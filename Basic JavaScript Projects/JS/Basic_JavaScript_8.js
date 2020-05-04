027509cd22fec52ac038a6f011701c2b1af611e6
BrandonF <37319101+zed71@users.noreply.github.com>
2020-05-04 14:28:33 -0700

    Add CSS, JS and HTML

--- Basic JavaScript Projects/JS/Basic_JavaScript_8.js
+++ Basic JavaScript Projects/JS/Basic_JavaScript_8.js
@@ -45,3 +45,15 @@ function myFunction() {
     var O = str.valueOf();
     document.getElementById("value").innerHTML = O;
 }
+
+function countdown() {
+    var seconds = document.getElementById("seconds").value; 
+
+    function tick();
+        seconds = seconds = 1;
+        timer.innerHTML = seconds;
+        setTimeout(tick, 1000);
+    if(seconds == -1){
+        alert("Time's up");
+    }
+}
